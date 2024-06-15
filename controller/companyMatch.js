7b825c8414e9009abf005b3c4d56ad84bbed2220
babith1234 <you@example.com>
2024-06-16 01:35:14 +0530

    matching partially done

--- controller/companyMatch.js
+++ controller/companyMatch.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 
 const Skill = require('../model/skillsModel')
-const techModel =require('../model/techModel')
+const Tech =require('../model/techModel')
 
 router.get('/fetchSkills',async(req, res)=>{
   try{
@@ -22,13 +22,30 @@ router.get('/fetchSkills',async(req, res)=>{
     // Extract top and bottom skills from the result
     const { topSkills, bottomSkills } = result[0];
 
-    // Send response
-    res.status(200).json({
-      status: 200,
-      message: 'Top and bottom skills fetched successfully',
-      data: { topSkills, bottomSkills },
-      success: true,
-    });
+   // Fetch all skills from the Tech model
+   const techSkills = await Tech.find();
+  //  console.log(techSkills)
+   const techSkillsSet = new Set(techSkills.map(t => t.skill));
+
+   console.log(techSkillsSet)
+
+   // Remove matching skills from topSkills
+   topSkills = topSkills.filter(skill => !techSkillsSet.has(skill.name));
+
+   // Remove matching skills from bottomSkills
+   bottomSkills = bottomSkills.filter(skill => !techSkillsSet.has(skill.name));
+
+   // Create a new object with the remaining skills from topSkills and original skills from topSkills
+   const newSkills = [...topSkills, ...bottomSkills];
+
+   // Send response
+   res.status(200).json({
+     status: 200,
+     message: 'Top and bottom skills fetched and filtered successfully',
+     data: { newSkills, topSkills, bottomSkills },
+     success: true,
+   });
+
 
   }catch(error){
     res.status(500).json({
