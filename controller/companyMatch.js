77561aefaeb4426742ea7a2857b6d4cd12f83f3f
babith1234 <you@example.com>
2024-06-16 00:23:10 +0530

    positve and negative skills

---
+++ controller/companyMatch.js
@@ -0,0 +1,44 @@
+const express = require('express')
+const router = express.Router
+
+const Skill = require('../model/skillsModel')
+
+router.get('/fetchSkills',async(req, res)=>{
+  try{
+
+    const aggregationPipeline = [
+      { $sort: { score: -1 } }, // Sort by score in descending order
+      { $facet: {
+          topSkills: [ { $limit: 3 } ],
+          bottomSkills: [ { $limit: 3 }, { $sort: { score: 1 } } ]
+        }
+      }
+    ];
+
+    // Execute the aggregation pipeline using the Skill model
+    const result = await Skill.aggregate(aggregationPipeline);
+
+    // Extract top and bottom skills from the result
+    const { topSkills, bottomSkills } = result[0];
+
+    // Send response
+    res.status(200).json({
+      status: 200,
+      message: 'Top and bottom skills fetched successfully',
+      data: { topSkills, bottomSkills },
+      success: true,
+    });
+
+  }catch(error){
+    res.status(500).json({
+      status: 500,
+      message: 'Internal server error',
+      error: error.message,
+      success: false,
+    });
+  }
+
+
+})
+
+module.exports = router
---
+++ model/skillsModel.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+// Define schema for the skills collection
+const skillSchema = new mongoose.Schema({
+  name: String,
+  score: Number
+});
+
+// Create a model based on the schema
+const Skill = mongoose.model('Skills', skillSchema);
+
+module.exports = Skill; // Export the model to be used elsewhere
